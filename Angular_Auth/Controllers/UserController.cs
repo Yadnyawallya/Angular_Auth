3ec7921e04633948ed97f243c6bec4356e51179f
yadnyawallya <yadnyawallyagawande@gmail.com>
2023-12-01 17:44:40 +0530

    Password Hasing and login

--- Angular_Auth/Controllers/UserController.cs
+++ Angular_Auth/Controllers/UserController.cs
@@ -1,8 +1,16 @@
 ﻿using Angular_Auth.Context;
+using Angular_Auth.Helper;
 using Angular_Auth.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Text;
+using System.Text.RegularExpressions;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+
 
 namespace Angular_Auth.Controllers
 {
@@ -22,12 +30,27 @@ namespace Angular_Auth.Controllers
             if (UserObj == null)
                 return BadRequest();
             var user = await _authContext.Users.
-                FirstOrDefaultAsync(x => x.UserName == UserObj.UserName && x.password==UserObj.password);
+                //FirstOrDefaultAsync(x => x.UserName == UserObj.UserName && x.password==UserObj.password);     
+                FirstOrDefaultAsync(x => x.UserName == UserObj.UserName);
+
             if (user == null)
-                return NotFound(new {Message="User Not Found"});
+                return NotFound(new { Message = "User Not Found!" });
+
+
+            if (!PasswordHasher.VerifyPassword(UserObj.password, user.password))
+            {
+                return BadRequest(new { Messgage = "password is Incorrect" });
+
+            }
+
+
+            user.Token = CreateJWT(user);
+
             return Ok(new
             {
-                Message ="Login Success!"
+                Token = user.Token,
+
+                Message = "Login Success!"
             });
         }
         [HttpPost("register")]  
@@ -35,12 +58,72 @@ namespace Angular_Auth.Controllers
         {
             if (UserObj==null)
                 return BadRequest();
+            //check Username
+            if (await CheckUserNameExistAsync(UserObj.UserName))
+                return BadRequest(new { Message = " user already Exists" });
+
+            //check Email 
+            if (await CheckEmailExistAsync(UserObj.Email))
+                return BadRequest(new { Message = " Email already Exists" });
+
+            //check Password Strength
+            var pass = CheckPasswordStrength(UserObj.password);
+            if (!string.IsNullOrEmpty(pass))
+                return BadRequest(new { Message = pass.ToString()});
+
+            UserObj.password = PasswordHasher.HashPassword(UserObj.password);
+            UserObj.Role = "user";
+            UserObj.Token = "";
             await _authContext.Users.AddAsync(UserObj);
             await _authContext.SaveChangesAsync();
             return Ok(new
-            {
+            { 
                 Message="User Register!"
             });
         }
+
+        private  Task<bool> CheckUserNameExistAsync(string UserName) 
+            => _authContext.Users.AnyAsync(x => x.UserName == UserName);
+
+        private Task<bool> CheckEmailExistAsync(string Email)
+            => _authContext.Users.AnyAsync(x => x.Email == Email);
+        private string CheckPasswordStrength(string password)
+        {
+            StringBuilder sb = new StringBuilder();
+            if (password.Length < 8)
+                sb.Append("Minimum password length should be 8" + Environment.NewLine);
+            if (!(Regex.IsMatch(password, "[a-z]") && Regex.IsMatch(password, "[A-Z]")
+                && Regex.IsMatch(password, "[0-9]")))
+                sb.Append("password should be alphanumeric" + Environment.NewLine);
+            if (!Regex.IsMatch(password, "[<,>,!,@,#,$,%,^,&,*,(,),_,+,\\[,\\],{,},?,:,;,|,',\\,.,/,~,`,-,=]"))
+                sb.Append("password should contain Special characters" + Environment.NewLine);
+            return sb.ToString();
+            
+        }
+
+        private string CreateJWT(Users user)
+        {
+            var jwtTokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes("veryverySecret.....");
+            var identity = new ClaimsIdentity(new Claim[]
+            {
+                new Claim(ClaimTypes.Role, user.Role),
+                new Claim(ClaimTypes.Name,$"{user.FirstName}{user.LastName}")
+
+            });
+
+            var credentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = identity,
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = credentials
+            };
+            var toekn = jwtTokenHandler.CreateToken(tokenDescriptor);
+            return jwtTokenHandler.WriteToken(toekn);
+        }
+
+
     }
 }
--- Angular_Auth/Program.cs
+++ Angular_Auth/Program.cs
@@ -1,5 +1,8 @@
 using Angular_Auth.Context;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,9 +12,35 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(option =>
+{
+    option.AddPolicy("MyPolicy", builder =>
+    {
+        builder.AllowAnyOrigin()
+        .AllowAnyMethod()
+        .AllowAnyHeader();
+    });
+});
 builder.Services.AddDbContext<AppDbcontext>(option =>
 {
     option.UseSqlServer(builder.Configuration.GetConnectionString("SqlServerConnStr"));
+});
+
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(x =>
+{
+    x.RequireHttpsMetadata = false;
+    x.SaveToken = true;
+    x.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey    = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("veryverySecret.....")),
+        ValidateAudience = false,
+        ValidateIssuer = false,
+    };
 });
 
 var app = builder.Build();
@@ -24,6 +53,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseCors("MyPolicy");
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
